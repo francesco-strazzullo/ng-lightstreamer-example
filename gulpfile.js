0f4a87cef683b3af35a08bf4ab88a994bfc65478
Francesco Strazzullo <me@francescostrazzullo.info>
2015-10-16 20:43:35 +0200

    added wiredep integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
- 
+var wiredep = require('wiredep');
+
 gulp.task('serve', function() {
   connect.server({
     livereload: true,
-    port:8000
+    port: 8000
+  });
+});
+
+gulp.task('wiredep', function() {
+  wiredep({
+    src: './index.html'
   });
 });

9525ffd0d6a76f07291f547e2069f662b3f3e974
francesco-strazzullo <me@francescostrazzullo.info>
2015-11-09 23:53:48 +0100

    using lighstreamer client to connect to demo data

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@
 
     angular.module('ng-lightstreamer-example', [
         'ng-lightstreamer'
-    ]);
+    ]).config(['lightstreamerProvider',function(lightstreamerProvider){
+    	lightstreamerProvider.setConfiguration({
+    		server:'http://push.lightstreamer.com',
+    		adapter:'DEMO'
+    	});
+    }]);
 
 }());
--- src/controllers.js
+++ src/controllers.js
@@ -1,8 +1,19 @@
 (function() {
     "use strict";
 
-    angular.module('ng-lightstreamer-example').controller('Main',['$scope',function($scope){
-    	
-    }]);
+    angular.module('ng-lightstreamer-example').controller('Main',[
+    	'$scope',
+    	'lightstreamer',
+    	function($scope,lightstreamer){
+
+    		$scope.status = "Waiting";
+
+    		lightstreamer.addStatusChangeListener(function(status){
+				$scope.status = status;
+			});
+
+    		$scope.connect = lightstreamer.connect;
+    	}
+    ]);
 
 }());
